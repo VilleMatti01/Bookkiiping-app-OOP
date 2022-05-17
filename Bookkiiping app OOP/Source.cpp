8dbecb7702d3c1f42c51bd520f0c1a927ce8605c
v1ll3m <Ville-Matti_S@hotmail.com>
2022-05-17 15:24:45 +0300

    1. reformatted createBudget(); 2. added netIncome to createBudget(); 3. created a way to Show how much user saves money 4. Removed totalBills 5. removed CalctotalMonthsSpending(); and move the calculations to the addExpense(); 6. fixed a bug so that addExpense(); chooses the right bills 7. Made addExpense(); Show right index numbers. 8. Fixed the way addExpense(); adds expenses to vector 9. Moved backToMainMenu(); in viewBudget(); in to the switch 10. in viewBudget(); added whitespace between "spend" and totalMonthsSpending; 11. added "bill" after the otherExpensesVectorString[i] in addExpense(); when its asking what bill user paid. 12. Removed int countOtherExpenses = 3; 13. removed int countBills;

--- Bookkiiping app OOP/Source.cpp
+++ Bookkiiping app OOP/Source.cpp
@@ -54,9 +54,12 @@ int    inputInt;
 double totalBudgetSum;
 double totalBills;
 double totalMonthsSpending = 0.00;
+double netIncome;
+double fuelKilometers;
+double fuelUsage;
+double fuelCost;
+double fuelTotal;
 int countIndex = 0;
-int countBills = 0;
-int countOtherExpenses = 3;
 int    mainMenu();
 int    createBudget();
 int    viewBudget();
@@ -64,21 +67,66 @@ int    incomeInfo();
 int    backToMainMenu();
 int    devTerminal();
 int    addExpense();
-int    calcTotalMonthsSpending();
-
+int	   calcCarFuelCost();
+int    calcOtherTransportCost();
+int    addExtraExpense();
 int main()
 {
 	int fillVector();
 	{
 		otherExpensesVectorString.push_back("Housing");
 		otherExpensesVectorString.push_back("Food");
-		otherExpensesVectorString.push_back("Transport");
 	}
 
 	mainMenu();
 	return 0;
 }
+
 //Modular functions that can be used in multiple menus.
+int	calcCarFuelCost()
+{
+	otherExpensesVectorString.push_back("Fuel");
+
+	cout << "How many kilometers do you drive in a week?" << endl;
+	cout << "kilometers: ";
+	cin >> fuelKilometers;
+	cin.get();
+
+	cout << "What is the average fuel consumption in liters per 100km for your car?" << endl;;
+	cout << "Liters: ";
+	cin >> fuelUsage;
+	cin.get();
+
+	cout << "How much did the fuel cost at the gas station per litre last time you where there?" << endl;
+	cout << "cost: ";
+	cin >> fuelCost;
+	cin.get();
+
+	fuelTotal = ((fuelKilometers / 100) * fuelUsage * fuelCost) * 4;
+	otherExpensesVectorDouble.push_back(fuelTotal);
+
+	cout << otherExpensesVectorDouble[2] << "e saved for fuel in monthly budget." << endl;
+	return 0;
+}
+int calcOtherTransportCost()
+{
+	cout << "What transport do you use?" << endl;
+	cout << "Transport: ";
+	getline(cin, inputString);
+
+	otherExpensesVectorString.push_back(inputString);
+
+	cout << "How much the " << otherExpensesVectorString[2] << " trips cost you in total on weekly basis?" << endl;
+	cout << "Cost: ";
+	cin >> inputDouble;
+	cin.get();
+
+	inputDouble *= 4;
+	otherExpensesVectorDouble.push_back(inputDouble);
+
+	cout << otherExpensesVectorDouble[2] << "e saved for " << otherExpensesVectorString[2] << "trips in monthly budget" << endl;
+	return 0;
+}
 int backToMainMenu()
 {
 	char inputChar;
@@ -106,16 +154,20 @@ int addExpense()
 	{
 		for (int i = 0; i < billsVectorString.size(); i++)
 		{
-			cout << i << ". " << billsVectorString[i] << endl; // please delete this e it should not be there. (DONE)
+			countIndex++;
+			cout << countIndex << ". " << billsVectorString[i] << endl; // please delete this e it should not be there. (DONE)
 		}
-
+		countIndex = 0;
 		cout << "Type the number of the bill you paid: ";
 		cin >> inputInt;
+		inputInt -= 1;
 		cin.get();
-		cout << "How much you paid for " << billsVectorString[inputInt] << "?: ";
+		cout << "How much you paid for " << billsVectorString[inputInt] << " bill?: ";
 		cin >> inputDouble; // (BUG) program crashes here probably because you are trying to insert a value to an non existant vector index. (FIXED)
 		cin.get();
-		billsSpendVectorDouble.insert(billsSpendVectorDouble.begin() + inputInt, inputDouble);
+		
+		billsSpendVectorDouble.at(inputInt) = inputDouble;
+		totalMonthsSpending += billsSpendVectorDouble[inputInt];
 		cout << billsVectorString[inputInt] << ": " << billsSpendVectorDouble[inputInt] << "e saved." << endl;
 	}
 
@@ -123,15 +175,19 @@ int addExpense()
 	{
 		for (int i = 0; i < otherExpensesVectorString.size(); i++)
 		{
-			cout << i << ". " << otherExpensesVectorString[i] << endl; // please delete this e it should not be there. (DONE)
+			countIndex++;
+			cout << countIndex << ". " << otherExpensesVectorString[i] << endl; // please delete this e it should not be there. (DONE)
 		}
+		countIndex = 0;
 		cout << "Type the number that corresponds rent, food or transportation: ";
 		cin >> inputInt;
+		inputInt -= 1;
 		cin.get();
 		cout << "how much you paid for " << otherExpensesVectorString[inputInt] << "?: ";
 		cin >> inputDouble;
 		cin.get();
-		otherExpensesSpendVectorDouble.insert(otherExpensesSpendVectorDouble.begin() + inputInt, inputDouble);
+		otherExpensesSpendVectorDouble.at(inputInt) = inputDouble;
+		totalMonthsSpending += otherExpensesSpendVectorDouble[inputInt];
 		cout << otherExpensesVectorString[inputInt] << ": " << otherExpensesSpendVectorDouble[inputInt] << "e saved" << endl;
 	}
 
@@ -149,17 +205,34 @@ int addExpense()
 	
 	return 0;
 }
-int calcTotalMonthsSpending()
+int addExtraExpense()
 {
-	for (int i = 0; i < billsSpendVectorDouble.size(); i++)
-	{
-		totalMonthsSpending += billsSpendVectorDouble[i];
-	}
+	cout << "EXTRA EXPENSES" << endl;
 
-
-	for (int i = 0; i < otherExpensesSpendVectorDouble.size(); i++)
+	while (inputChar != 'q')
 	{
-		totalMonthsSpending += otherExpensesSpendVectorDouble[i];
+		cout << "Name of the expense: ";
+		getline(cin, inputString);
+
+		otherExpensesVectorString.push_back(inputString);
+
+		cout << "how much does " << otherExpensesVectorString.back() << " cost you per month?" << endl;
+		cout << "Monthly cost: ";
+		cin >> inputDouble;
+		cin.get();
+
+		otherExpensesVectorDouble.push_back(inputDouble);
+		
+		cout << otherExpensesVectorString.back() << ": " << otherExpensesVectorDouble.back() << "e/month saved." << endl;
+		cout << endl;
+
+		cout << "Do you want to" << endl;
+		cout << "A) Add another expense." << endl;
+		cout << "Q) stop adding expenses. " << endl;
+		cout << "input: ";
+		cin >> inputChar;
+		cout << endl;
+
 	}
 	return 0;
 }
@@ -225,23 +298,39 @@ int createBudget()
 	cout << endl;
 	cout << "A) CREATE A BUGDET." << endl;
 	cout << "-------------------" << endl << endl;
-	cout << "Here you can create yourself a monthly budget based on your must"<< endl << "have expenses. First we will go throw all the bills you have to"<< endl <<"pay every month. Just type the name of the bill and then how" << endl << "much it costs.then you type how much money you need every week for food" << endl << ", your rent and water bill if it is not included in your rent" << endl << " and lastly how much you need money for petrol / gasoline for car." << endl;
-	cout << "So Lets go over your bills. First type the name of the bill and then the amount. When you have included all your bills type Q to exit" << endl;
-
-
+	cout << "Here you can create yourself a monthly budget based on your must" << endl <<
+		"have expenses. First you save the amount of net-income(after taxes" << endl <<
+		"income, The green+ you get to your bank account every month), Second" << endl <<
+		"We go through all the bills you have to pay on monthly basis, third" << endl <<
+		"you tell the program the total sum of your housing costs(water," << endl <<
+		"payment, rent, homeload payment,car parking space),fourth how much" << endl <<
+		"Money you need for transportation and fifth how much money you" << endl <<
+		"need for food.This is only a guide line but it is strongly adviced" << endl <<
+		"that you stick to your budget. Now with that out of the way,lets start!" << endl << endl;
+			
+	cout << "How much is your monthly net-income?: ";
+	cin >> netIncome;
+	cin.get();
+	cout << "Net-income " << netIncome << "e saved." << endl;
 	cout << endl;
-	cout << "Name: ";
+
+	cout << "BILLS" << endl;
+	cout << "Name of the bill: ";
 	getline(cin, inputString);
 	billsVectorString.push_back(inputString);
 
 	cout << "bill amount: ";
 	cin >> inputDouble;
 	cin.get();
-	cout << endl;
+
+	countIndex++;
 	billsVectorDouble.push_back(inputDouble);
 	billsSpendVectorDouble.push_back(0.0);
-	cout << billsVectorString[count]<< " " << billsVectorDouble[count] << "e saved " << endl;
-	cout << "Type a to add another bill: ";
+
+	cout <<countIndex<<". " << billsVectorString[count] << " " << billsVectorDouble[count] << "e saved " << endl;
+	cout << endl;
+
+	cout << "Press A to add another bill,Press Q to quit adding bills: ";
 	cin >> inputChar;
 	cin.get();
 	cout << endl;
@@ -250,69 +339,103 @@ int createBudget()
 	while (inputChar != 'q') //  BUG REPORT: while user is typing bill name and if user types it like this -> "car insurance" and not like this "carInsurance" it will cause infinite loop. (FIXED!)
 	{
 		count++;
-		cout << "bill name: ";
+		cout << "Name of the bill: ";
 		getline(cin, inputString);
 		billsVectorString.push_back(inputString);
 
 		cout << billsVectorString[count] << " bill amount: ";
 		cin >> inputDouble;
 		cin.get();
-		cout << endl;
+
+		countIndex++;
 		billsVectorDouble.push_back(inputDouble);
 		billsSpendVectorDouble.push_back(0.0);
-		countBills++;
-		cout << billsVectorString[count] <<" "<< billsVectorDouble[count] << "e " << "saved. " << endl;
-		cout << "Press A to add another bill And Q to quit adding bills and press enter: ";
+		
+		cout << countIndex <<". " << billsVectorString[count] << " " << billsVectorDouble[count] << "e " << "saved. " << endl;
+		cout << endl;
+
+		cout << "Press A to add another bill,Press Q to quit adding bills: ";
 		cin >> inputChar;
 		cin.get();
 		cout << endl;
 	} 
+	countIndex = 0;
 
-
-	cout << endl;
-	cout << "next, how much is the sum of all your housing costs (water/rent/car parking/home loan payment) payment every month: ";
-	cin >> inputDouble;
-	otherExpensesVectorDouble.push_back(inputDouble);
-	otherExpensesSpendVectorDouble.push_back(0.0);
-	cout << endl;
-	cout << "how much you need money for food everyweek?: ";
-	cin >> inputDouble;
-	inputDouble *= 4;
-	otherExpensesVectorDouble.push_back(inputDouble);
-	otherExpensesSpendVectorDouble.push_back(0.0);
-	cout << "How much money do you need for transport(Bus Tickets, Petrol etc) every week?: ";
-	cin >> inputDouble;
-	inputDouble *= 4;
-	otherExpensesVectorDouble.push_back(inputDouble);
-	otherExpensesSpendVectorDouble.push_back(0.0);
-	cout << endl;
-	cout << "YOUR MONTHLY BUDGET" << endl;
-	cout << "-------------------" << endl << endl;
-	cout << "BILLS" << endl;
-
+	cout << "HOUSING" << endl;
+	cout << "How much is the sum of all of your housing costs (water/rent/car parking/home loan) payment every month?" <<endl;
 	
+	cout << "Cost: ";
+	cin >> inputDouble;
+	cin.get();
+
+	otherExpensesVectorDouble.push_back(inputDouble);
+	otherExpensesSpendVectorDouble.push_back(0.0);
+	cout << endl;
+
+	cout << "FOOD" << endl;
+	cout << "how much do you need money for food everyweek?" << endl;
+	cout << "Amount: ";
+	cin >> inputDouble;
+	cin.get();
+
+	inputDouble *= 4;
+	otherExpensesVectorDouble.push_back(inputDouble);
+	otherExpensesSpendVectorDouble.push_back(0.0);
+	cout << endl;
+
+	cout << "TRANSPORT" << endl;
+	cout << "For transport, do you use " << endl;
+	cout << "A) a car." << endl << "B) Something else." << endl;
+
+	cout << "input: ";
+	cin >> inputChar;
+	cin.get();
+	switch (inputChar)
+	{
+	case 'a':
+		calcCarFuelCost();
+		break;
+	case 'A':
+		calcCarFuelCost();
+		break;
+	case 'b':
+		calcOtherTransportCost();
+		break;
+	case 'B':
+		calcOtherTransportCost();
+		break;
+	default:
+		cout << "ERROR 506. You entered invalid character" << endl;
+		break;
+	}
+	cout << endl;
+
+	cout << "Do you have any other expenses that were not mentioned above that you must take care of?" << endl;
+
+	cout << "A) Yes i have." << endl << "B) No i dont." << endl;
+	cout << "input: ";
+	cin >> inputChar;
+	cin.get();
+
+	if (inputChar == 'a' || inputChar == 'A')
+	{
+		addExtraExpense();
+	}
+
+
+
+	cout << "YOUR MONTHLY BUDGET" << endl;
+	cout << "-------------------" << endl;
+
 	for (int i = 0; i < billsVectorDouble.size(); i++) 	
 	{
 		countIndex++;
 		cout << countIndex << ". " << billsVectorString[i] << ": " << billsVectorDouble[i] << "e " << endl;
 	}
-	
-
-	for (int i = 0; i < billsVectorDouble.size(); i++)
-	{
-		totalBills += billsVectorDouble[i];
-	}
-
-
-	cout << "-------------" << endl;
-	cout << "Total bills: " << totalBills << "e " << endl;
-	cout << endl;
-
-
 	for (int i = 0; i < otherExpensesVectorDouble.size(); i++)
 	{
 		countIndex++;
-		cout << countIndex << ". " << otherExpensesVectorString[i] << otherExpensesVectorDouble[i] << "e " << endl;
+		cout << countIndex << ". " << otherExpensesVectorString[i] <<": " << otherExpensesVectorDouble[i] << "e " << endl;
 	}
 	countIndex = 0;
 
@@ -320,8 +443,6 @@ int createBudget()
 	{
 		totalBudgetSum += billsVectorDouble[i];
 	}
-
-
 	for (int i = 0; i < otherExpensesVectorDouble.size(); i++)
 	{
 		totalBudgetSum += otherExpensesVectorDouble[i];
@@ -329,8 +450,9 @@ int createBudget()
 
 
 	cout << "----------------------" << endl;
-	cout << "Total monthly bugdet: " << totalBudgetSum << "e " << endl << endl;
-
+	cout << "Your monthly net-income is: " << netIncome << "e." << endl;
+	cout << "Total monthly bugdet: " << totalBudgetSum << "e." << endl;
+	cout << "You can save " << netIncome - totalBudgetSum << "e." << endl;
 
 	backToMainMenu();
 
@@ -340,7 +462,6 @@ int viewBudget()
 {
 
 
-	calcTotalMonthsSpending();
 
 	cout << "B) VIEW BUDGET/ SPENDING." << endl;
 	cout << "-------------------------" << endl<< endl;
@@ -363,7 +484,6 @@ int viewBudget()
 	cout << "Monthly budget: " << totalBudgetSum << "e " << endl;
 	cout << endl;
 
-	// THIS GOES TO SWITCH TABLES CASE 'b'! START
 	cout << "Your spending this month" << endl;
 	cout << "------------------------" << endl;
 	cout << endl;
@@ -381,11 +501,11 @@ int viewBudget()
 	}
 	countIndex = 0;
 	cout << "--------------" << endl;
-	cout << "You have spend:" << totalMonthsSpending << "e this month." << endl; 
+	cout << "You have spend: " << totalMonthsSpending << "e this month." << endl; 
 	cout << endl;
-	// THIS GOES TO SWITCH TABLES CASE 'b'! END
+
 	cout << "A) Add an expense" << endl;
-	cout << "B) view expenses and how much of budget is left." << endl;
+	cout << "B) Back to main menu." << endl;
 	cin >> inputChar;
 	switch (inputChar)
 	{
@@ -397,18 +517,16 @@ int viewBudget()
 	    break;
 
 	case 'b':
-		addExpense();
+		mainMenu();
 		break;
 	case 'B':
-		addExpense();
+		mainMenu();
 		break;
 
 
 	default:
 		break;
 	}
-
-	backToMainMenu();
 
 
 	return 0;
